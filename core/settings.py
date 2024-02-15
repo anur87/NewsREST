9d65dd58113a93a1abfef04b99cbcf97bc1b5d53
anur87 <anuardosmukhamed87@gmail.com>
2024-02-15 13:27:52 +0600

    Second commit

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG')))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS.split(',') if host.strip()]
 
 
 
